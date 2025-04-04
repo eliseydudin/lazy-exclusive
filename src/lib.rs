202475d6bb918b4104c6ad715787b0886c7f6f08
eliseydudin <dudinelisey630@gmail.com>
2025-04-04 20:57:46 +0300

    add the ability to use locks

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,28 @@
+#[cfg(feature = "use-locks")]
+use lock::Lock;
 use std::{
     cell::{Cell, UnsafeCell},
     ops::{Deref, DerefMut},
 };
 
-#[derive(Clone, Copy)]
-enum State {
+#[cfg(feature = "use-locks")]
+use std::ptr;
+#[cfg(feature = "use-locks")]
+mod lock;
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum State {
     Unlocked,
     Locked,
+    #[cfg(feature = "use-locks")]
+    Poisoned,
 }
 
 pub struct LazyExclusive<T> {
     state: Cell<State>,
     data: UnsafeCell<T>,
+    #[cfg(feature = "use-locks")]
+    lock: UnsafeCell<Option<Lock>>,
 }
 
 unsafe impl<T> Send for LazyExclusive<T> {}
@@ -36,6 +47,17 @@ impl<T> Mut<'_, T> {
 impl<T> Drop for Mut<'_, T> {
     fn drop(&mut self) {
         self.source.state.set(State::Unlocked);
+        #[cfg(feature = "use-locks")]
+        {
+            self.source
+                .get_lock()
+                .as_ref()
+                .inspect(|lock| lock.unlock());
+
+            if std::thread::panicking() {
+                self.source.state.set(State::Poisoned)
+            }
+        }
     }
 }
 
@@ -64,17 +86,72 @@ impl<T> LazyExclusive<T> {
         let data = UnsafeCell::new(data);
         let state = Cell::new(State::Unlocked);
 
-        Self { state, data }
+        #[cfg(not(feature = "use-locks"))]
+        return Self { state, data };
+        #[cfg(feature = "use-locks")]
+        Self {
+            state,
+            data,
+            lock: UnsafeCell::new(None),
+        }
     }
 
     pub fn get(&self) -> Option<Mut<'_, T>> {
+        #[cfg(feature = "use-locks")]
+        let lock = self.get_lock();
+        #[cfg(feature = "use-locks")]
+        if lock.is_none() {
+            *lock = Some(Lock::new())
+        }
+
         match self.state.get() {
             State::Unlocked => {
                 self.state.set(State::Locked);
+                #[cfg(feature = "use-locks")]
+                lock.as_ref().inspect(|l| l.lock());
                 Some(Mut { source: self })
             }
-            State::Locked => None,
+            _ => None,
         }
+    }
+
+    #[cfg(feature = "use-locks")]
+    fn get_lock(&self) -> &mut Option<Lock> {
+        unsafe { self.lock.get().as_mut() }.unwrap()
+    }
+
+    pub fn swap(&self, new_value: T) {
+        unsafe {
+            let t = self.data.get().as_mut().unwrap();
+            *t = new_value;
+            self.state.set(State::Locked);
+
+            #[cfg(feature = "use-locks")]
+            {
+                let lock = self.lock.get().as_mut().unwrap();
+                if lock.is_some() {
+                    ptr::drop_in_place(self.lock.get());
+                }
+                *lock = None;
+            }
+        }
+    }
+
+    pub fn get_state(&self) -> State {
+        self.state.get()
+    }
+
+    #[cfg(feature = "use-locks")]
+    pub fn wait(&self) -> Mut<'_, T> {
+        let lock = self.get_lock();
+        if lock.is_none() {
+            *lock = Some(Lock::new())
+        }
+
+        lock.as_ref().expect("Lock should exist").lock();
+        assert_eq!(self.state.get(), State::Unlocked, "The data was poisoned");
+        self.state.set(State::Locked);
+        Mut { source: self }
     }
 }
 
@@ -99,5 +176,24 @@ mod tests {
         static SHARED: LazyExclusive<i32> = LazyExclusive::new(1231);
         let pointer = SHARED.get().unwrap();
         assert_eq!(*pointer, 1231);
+    }
+
+    #[cfg(feature = "use-locks")]
+    #[test]
+    fn lock_test() {
+        use crate::State;
+        use std::time::Duration;
+
+        static SHARED: LazyExclusive<i32> = LazyExclusive::new(120);
+        let mut lock = SHARED.get().unwrap();
+
+        std::thread::spawn(move || {
+            *lock *= 2;
+            std::thread::sleep(Duration::new(5, 0));
+        });
+
+        assert_eq!(SHARED.get_state(), State::Locked);
+        let new_lock = SHARED.wait();
+        assert_eq!(*new_lock, 120 * 2);
     }
 }
---
+++ src/lock.rs
@@ -0,0 +1,91 @@
+#![allow(non_camel_case_types)]
+
+use std::{cell::UnsafeCell, ptr};
+
+#[cfg(target_os = "windows")]
+type SRWLOCK = usize;
+
+#[cfg(target_os = "windows")]
+unsafe extern "C" {
+    fn InitializeSRWLock(lock: *mut SRWLOCK);
+    fn AcquireSRWLockExclusive(lock: *mut SRWLOCK);
+    fn ReleaseSRWLockExclusive(lock: *mut SRWLOCK);
+}
+
+#[cfg(all(target_pointer_width = "64", not(target_os = "windows")))]
+type PTHREAD_MUTEX_T = [u8; 40];
+#[cfg(all(target_pointer_width = "64", not(target_os = "windows")))]
+const LEN: usize = 40;
+
+#[cfg(all(target_pointer_width = "32", not(target_os = "windows")))]
+type PTHREAD_MUTEX_T = [u8; 24];
+#[cfg(all(target_pointer_width = "32", not(target_os = "windows")))]
+const LEN: usize = 24;
+
+#[cfg(not(target_os = "windows"))]
+#[link(name = "pthread")]
+unsafe extern "C" {
+    fn pthread_mutex_init(lock: *mut PTHREAD_MUTEX_T, attr: *const u8) -> i32;
+    fn pthread_mutex_lock(lock: *mut PTHREAD_MUTEX_T) -> i32;
+    fn pthread_mutex_unlock(lock: *mut PTHREAD_MUTEX_T) -> i32;
+    fn pthread_mutex_destroy(lock: *mut PTHREAD_MUTEX_T) -> i32;
+}
+
+pub struct Lock {
+    #[cfg(not(target_os = "windows"))]
+    data: UnsafeCell<PTHREAD_MUTEX_T>,
+    #[cfg(target_os = "windows")]
+    data: UnsafeCell<SRWLOCK>,
+}
+
+impl Lock {
+    pub fn new() -> Self {
+        #[cfg(not(target_os = "windows"))]
+        let data = unsafe {
+            let data = UnsafeCell::new([0_u8; LEN]);
+            let result = pthread_mutex_init(data.get(), ptr::null());
+            assert_eq!(
+                result, 0,
+                "Cannot initialize the mutex: `pthread_mutex_init` returned a non-zero value"
+            );
+            data
+        };
+        #[cfg(target_os = "windows")]
+        let data = unsafe {
+            let cell = UnsafeCell::new(0 as SRWLOCK);
+            InitializeSRWLock(cell.get());
+            cell
+        };
+
+        Self { data }
+    }
+
+    pub fn lock(&self) {
+        #[cfg(not(target_os = "windows"))]
+        unsafe {
+            pthread_mutex_lock(self.data.get())
+        };
+        #[cfg(target_os = "windows")]
+        unsafe {
+            AcquireSRWLockExclusive(self.data.get())
+        }
+    }
+
+    pub fn unlock(&self) {
+        #[cfg(not(target_os = "windows"))]
+        unsafe {
+            pthread_mutex_unlock(self.data.get())
+        };
+        #[cfg(target_os = "windows")]
+        unsafe {
+            ReleaseSRWLockExclusive(self.data.get())
+        }
+    }
+}
+
+#[cfg(not(target_os = "windows"))]
+impl Drop for Lock {
+    fn drop(&mut self) {
+        unsafe { pthread_mutex_destroy(self.data.get()) };
+    }
+}
